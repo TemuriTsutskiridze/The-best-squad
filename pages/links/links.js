56702cd6a47c644de8cfc4e8a4bb04e3ea167aed
Achi Phutkaradze <achifutkaradze9@gmail.com>
2023-08-29 19:28:15 +0400

    full tablet-version

--- pages/links/links.js
+++ pages/links/links.js
@@ -2,8 +2,11 @@ const addButton = document.getElementById("add");
 
 addButton.addEventListener("click", function () {
   document.querySelector(".main-container").style.display = "none";
+  document.querySelector(".mobile-version").style.display = "none";
+
   {
     document.querySelector(".container").style.display = "flex";
     document.getElementById("save").style.opacity = "1";
+    document.querySelector(".tablet-version").style.display = "block";
   }
 });
