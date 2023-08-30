18a1915a7c9f2eea8f006f5cb6017aef92a6897d
Achi Phutkaradze <achifutkaradze9@gmail.com>
2023-08-31 02:09:28 +0400

    full tablet-version and mobile-version

--- pages/links/links.js
+++ pages/links/links.js
@@ -2,11 +2,17 @@ const addButton = document.getElementById("add");
 
 addButton.addEventListener("click", function () {
   document.querySelector(".main-container").style.display = "none";
-  document.querySelector(".mobile-version").style.display = "none";
-
   {
-    document.querySelector(".container").style.display = "flex";
+    document.querySelector(".container").style.display = "block";
     document.getElementById("save").style.opacity = "1";
-    document.querySelector(".tablet-version").style.display = "block";
+  }
+});
+
+// console.log(document.querySelector(".mobile-version"));
+
+addButton.addEventListener("click", function () {
+  if (window.innerWidth >= 768) {
+    document.querySelector(".mobile-version").style.marginTop = "260px";
+    document.querySelector(".mobile-version").style.padding = "20px";
   }
 });
